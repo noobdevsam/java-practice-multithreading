5cdca883a9f71979810145dab2aad942ddce006d
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-29 12:44:52 +0600

    comment out thread creation and execution examples in Main class

--- src/main/java/thread_basics/Main.java
+++ src/main/java/thread_basics/Main.java
@@ -3,17 +3,17 @@ package thread_basics;
 public class Main {
 
     public static void main(String[] args) {
-        // Create a new thread using the MyThread class
-        MyThread myThread = new MyThread();
-
-        // Start the thread
-        myThread.start();
-
-        // Create a new thread using the MyRunnable class
-        Thread runnableThread = new Thread(new MyRunnable());
-
-        // Start the runnable thread
-        runnableThread.start();
+//        // Create a new thread using the MyThread class
+//        MyThread myThread = new MyThread();
+//
+//        // Start the thread
+//        myThread.start();
+//
+//        // Create a new thread using the MyRunnable class
+//        Thread runnableThread = new Thread(new MyRunnable());
+//
+//        // Start the runnable thread
+//        runnableThread.start();
 
         // Print the current thread's name
         System.out.println("Main thread: " + Thread.currentThread().getName());
