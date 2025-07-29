0476e76c0e0fe04b3ce7c35624cd7ab933c70eaa
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-29 13:24:24 +0600

    refactor Counter class to add thread-safe increment method and improve documentation

--- src/main/java/thread_basics/Counter.java
+++ src/main/java/thread_basics/Counter.java
@@ -2,10 +2,26 @@ package thread_basics;
 
 class Counter {
 
-    int count = 0;
+    static int count = 0;
 
-    public void increment() {
+    /**
+     * Increments the count variable and prints the current count.
+     * This method is not synchronized, which means it is not thread-safe.
+     * and occur race conditions if accessed by multiple threads concurrently.
+     */
+    void increment() {
         count++;
+        System.out.println("Count: " + count + " by " + Thread.currentThread().getName());
+    }
+
+    /**
+     * Increments the count variable in a thread-safe manner.
+     * This method is synchronized, which means it can be safely accessed by multiple threads concurrently.
+     * and prevents race conditions.
+     */
+    synchronized void increment_two() {
+        count++;
+        System.out.println("Count: " + count + " by " + Thread.currentThread().getName());
     }
 
 }
