a89e3b2c3cf03146b1755b1dea839e999036ab8c
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-29 12:48:55 +0600

    update main method to demonstrate concurrent execution with Task class

--- src/main/java/thread_basics/Main.java
+++ src/main/java/thread_basics/Main.java
@@ -2,7 +2,7 @@ package thread_basics;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        // Create a new thread using the MyThread class
 //        MyThread myThread = new MyThread();
 //
@@ -17,5 +17,16 @@ public class Main {
 
         // Print the current thread's name
         System.out.println("Main thread: " + Thread.currentThread().getName());
+
+        var tr1 = new Thread(new Task("Task 1"));
+        var tr2 = new Thread(new Task("Task 2"));
+
+        tr1.start();
+        tr2.start();
+
+        tr1.join(); // Wait for Task-1 (tr1) to finish
+        tr2.join(); // Wait for Task-2 (tr2) to finish
+
+        System.out.println("All tasks completed.");
     }
 }
