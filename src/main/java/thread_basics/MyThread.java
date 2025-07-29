591cebc21d69aefc200aa093fd6fb80c6f483560
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-29 12:17:27 +0600

    initial commit

---
+++ src/main/java/thread_basics/MyThread.java
@@ -0,0 +1,23 @@
+package thread_basics;
+
+public class MyThread extends Thread {
+
+    /**
+     * This method is run by the thread when it executes. Subclasses of {@code
+     * Thread} may override this method.
+     *
+     * <p> This method is not intended to be invoked directly. If this thread is a
+     * platform thread created with a {@link Runnable} task then invoking this method
+     * will invoke the task's {@code run} method. If this thread is a virtual thread
+     * then invoking this method directly does nothing.
+     *
+     * @implSpec The default implementation executes the {@link Runnable} task that
+     * the {@code Thread} was created with. If the thread was created without a task
+     * then this method does nothing.
+     */
+    @Override
+    public void run() {
+        System.out.println("Running in thread: " + Thread.currentThread().getName());
+    }
+
+}
