ecdd4107d6312e53bb2653ef405f9c4543e3ff12
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-29 12:33:48 +0600

    add runnable thread example using MyRunnable class

--- src/main/java/thread_basics/Main.java
+++ src/main/java/thread_basics/Main.java
@@ -9,6 +9,12 @@ public class Main {
         // Start the thread
         myThread.start();
 
+        // Create a new thread using the MyRunnable class
+        Thread runnableThread = new Thread(new MyRunnable());
+
+        // Start the runnable thread
+        runnableThread.start();
+
         // Print the current thread's name
         System.out.println("Main thread: " + Thread.currentThread().getName());
     }
