0b7b4fc38122ae5ac8e705524c7a282c5b0c80a6
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-30 00:59:58 +0600

    add simulate_ticket_booking method to test concurrent ticket booking functionality

--- src/main/java/third_phase/mini_projects/ticket_booking_system/TicketBookingTest.java
+++ src/main/java/third_phase/mini_projects/ticket_booking_system/TicketBookingTest.java
@@ -3,16 +3,24 @@ package third_phase.mini_projects.ticket_booking_system;
 public class TicketBookingTest {
 
     public static void main(String[] args) {
+        simulate_ticket_booking();
+    }
+
+    private static void simulate_ticket_booking() {
+        // Create an instance of the TicketBooking system
         var ticketBookingSystem = new TicketBooking();
 
+        // Define a task that books a ticket for the current thread's user
         Runnable task = () -> ticketBookingSystem.bookTicket(
                 Thread.currentThread().getName(), 4
         );
 
+        // Create threads representing different users booking tickets
         var thread1 = new Thread(task, "User-1");
         var thread2 = new Thread(task, "User-2");
         var thread3 = new Thread(task, "User-3");
 
+        // Start the threads to execute the ticket booking task concurrently
         thread1.start();
         thread2.start();
         thread3.start();
