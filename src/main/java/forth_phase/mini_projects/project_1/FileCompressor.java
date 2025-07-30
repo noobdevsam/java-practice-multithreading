d3a8ad887c48c1f604d9efd90ea27cce83c91fce
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-30 20:29:31 +0600

    refactor FileCompressor to implement directory compression and improve error handling

--- src/main/java/forth_phase/mini_projects/project_1/FileCompressor.java
+++ src/main/java/forth_phase/mini_projects/project_1/FileCompressor.java
@@ -2,9 +2,8 @@ package forth_phase.mini_projects.project_1;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -29,55 +28,34 @@ class FileCompressor {
         this.executor = Executors.newFixedThreadPool(numThreads);
     }
 
-//    /**
-//     * Compresses all regular files in the specified input directory and writes
-//     * the compressed files to the specified output directory.
-//     *
-//     * @param inputDirectory  the path to the directory containing files to compress
-//     * @param outputDirectory the path to the directory where compressed files will be stored
-//     * @throws IOException if an I/O error occurs while accessing the directories or files
-//     */
-//    public void compressDirectory(
-//            String inputDirectory,
-//            String outputDirectory
-//    ) throws IOException {
-//        // Create the output directory if it does not exist
-//        Files.createDirectories(Paths.get(outputDirectory));
-//
-//        // List all files in the input directory
-//        try (var files = Files.list(Paths.get(inputDirectory))) {
-//
-//            // Filter regular files and submit compression tasks to the executor
-//            files.filter(Files::isRegularFile)
-//                    .forEach(file -> {
-//                        var outputFile = Paths.get(outputDirectory, file.getFileName().toString() + ".gz");
-//                        executor.submit(new CompressionTask(file, outputFile));
-//                    });
-//
-//        } catch (NoSuchFileException ex) {
-//            logger.severe("No such directory available: " + ex.getMessage());
-//        }
-//    }
-
-
-    public void compressAllFilesToSingleGz(
+    /**
+     * Compresses all regular files in the specified input directory and writes
+     * the compressed files to the specified output directory.
+     *
+     * @param inputDirectory  the path to the directory containing files to compress
+     * @param outputDirectory the path to the directory where compressed files will be stored
+     * @throws IOException if an I/O error occurs while accessing the directories or files
+     */
+    public void compressDirectory(
             String inputDirectory,
-            String outputFile
+            String outputDirectory
     ) throws IOException {
-        var inputDirPath = Paths.get(inputDirectory);
-        var outputDirPath = Paths.get(outputFile);
+        // Create the output directory if it does not exist
+        Files.createDirectories(Paths.get(outputDirectory));
 
-        List<Path> inputFiles;
+        // List all files in the input directory
+        try (var files = Files.list(Paths.get(inputDirectory))) {
 
-        try (var files = Files.list(inputDirPath)) {
-            inputFiles = files
-                    .filter(Files::isRegularFile)
-                    .toList();
+            // Filter regular files and submit compression tasks to the executor
+            files.filter(Files::isRegularFile)
+                    .forEach(file -> {
+                        var outputFile = Paths.get(outputDirectory, file.getFileName().toString() + ".gz");
+                        executor.submit(new CompressionTask(file, outputFile));
+                    });
+
+        } catch (NoSuchFileException ex) {
+            logger.severe("No such directory available: " + ex.getMessage());
         }
-
-        executor.submit(
-                new CompressionTask(inputFiles, outputDirPath)
-        );
     }
 
     /**
