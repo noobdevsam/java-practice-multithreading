074a9fd1625250345593fbf8908d51145ae1812d
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-30 20:15:32 +0600

    add Main class to initiate file compression process

---
+++ src/main/java/forth_phase/mini_projects/project_1/Main.java
@@ -0,0 +1,41 @@
+package forth_phase.mini_projects.project_1;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        perform_compression();
+
+    }
+
+    private static void perform_compression() throws IOException, InterruptedException {
+
+        try {
+
+            var compressor = new FileCompressor(4);
+
+            var inputDir = "src/main/resources/project1/input";
+            var outputDir = "src/main/resources/project1/output";
+
+            var start = System.currentTimeMillis();
+
+            // compressor.compressDirectory(inputDir, outputDir);
+            compressor.compressAllFilesToSingleGz(inputDir, outputDir);
+
+            compressor.shutdown();
+
+            var end = System.currentTimeMillis();
+            logger.info("Compression completed in " + (end - start) + " milliseconds");
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+}
