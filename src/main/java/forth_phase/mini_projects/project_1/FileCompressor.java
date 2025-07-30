6cc0b3f514843a48374fc1637553ed3c0bd0033e
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-30 20:12:54 +0600

    add method to compress multiple files into a single GZ file

--- src/main/java/forth_phase/mini_projects/project_1/FileCompressor.java
+++ src/main/java/forth_phase/mini_projects/project_1/FileCompressor.java
@@ -2,10 +2,13 @@ package forth_phase.mini_projects.project_1;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 /**
  * The FileCompressor class provides functionality to compress files in a directory
@@ -14,6 +17,8 @@ import java.util.concurrent.TimeUnit;
 class FileCompressor {
 
     private final ExecutorService executor;
+
+    private final Logger logger = Logger.getLogger(FileCompressor.class.getName());
 
     /**
      * Constructs a FileCompressor with a specified number of threads.
@@ -24,32 +29,55 @@ class FileCompressor {
         this.executor = Executors.newFixedThreadPool(numThreads);
     }
 
-    /**
-     * Compresses all regular files in the specified input directory and writes
-     * the compressed files to the specified output directory.
-     *
-     * @param inputDirectory  the path to the directory containing files to compress
-     * @param outputDirectory the path to the directory where compressed files will be stored
-     * @throws IOException if an I/O error occurs while accessing the directories or files
-     */
-    public void compressDirectory(
+//    /**
+//     * Compresses all regular files in the specified input directory and writes
+//     * the compressed files to the specified output directory.
+//     *
+//     * @param inputDirectory  the path to the directory containing files to compress
+//     * @param outputDirectory the path to the directory where compressed files will be stored
+//     * @throws IOException if an I/O error occurs while accessing the directories or files
+//     */
+//    public void compressDirectory(
+//            String inputDirectory,
+//            String outputDirectory
+//    ) throws IOException {
+//        // Create the output directory if it does not exist
+//        Files.createDirectories(Paths.get(outputDirectory));
+//
+//        // List all files in the input directory
+//        try (var files = Files.list(Paths.get(inputDirectory))) {
+//
+//            // Filter regular files and submit compression tasks to the executor
+//            files.filter(Files::isRegularFile)
+//                    .forEach(file -> {
+//                        var outputFile = Paths.get(outputDirectory, file.getFileName().toString() + ".gz");
+//                        executor.submit(new CompressionTask(file, outputFile));
+//                    });
+//
+//        } catch (NoSuchFileException ex) {
+//            logger.severe("No such directory available: " + ex.getMessage());
+//        }
+//    }
+
+
+    public void compressAllFilesToSingleGz(
             String inputDirectory,
-            String outputDirectory
+            String outputFile
     ) throws IOException {
-        // Create the output directory if it does not exist
-        Files.createDirectories(Paths.get(outputDirectory));
+        var inputDirPath = Paths.get(inputDirectory);
+        var outputDirPath = Paths.get(outputFile);
 
-        // List all files in the input directory
-        try (var files = Files.list(Paths.get(inputDirectory))) {
+        List<Path> inputFiles;
 
-            // Filter regular files and submit compression tasks to the executor
-            files.filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        var outputFile = Paths.get(outputDirectory, file.getFileName().toString() + ".gz");
-                        executor.submit(new CompressionTask(file, outputFile));
-                    });
-
+        try (var files = Files.list(inputDirPath)) {
+            inputFiles = files
+                    .filter(Files::isRegularFile)
+                    .toList();
         }
+
+        executor.submit(
+                new CompressionTask(inputFiles, outputDirPath)
+        );
     }
 
     /**
