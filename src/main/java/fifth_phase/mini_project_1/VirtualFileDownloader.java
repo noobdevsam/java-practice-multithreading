dfd004e2f00eda0a0fde02b819794152fe1cc14a
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-03 21:00:41 +0600

    add IOException handling to simpleDownloadFile method in VirtualFileDownloader interface

--- src/main/java/fifth_phase/mini_project_1/VirtualFileDownloader.java
+++ src/main/java/fifth_phase/mini_project_1/VirtualFileDownloader.java
@@ -1,7 +1,9 @@
 package fifth_phase.mini_project_1;
 
+import java.io.IOException;
+
 public interface VirtualFileDownloader {
 
-    void simpleDownloadFile(String urlString);
+    void simpleDownloadFile(String urlString) throws IOException;
 
 }
